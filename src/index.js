c661c9db948ecbbb8219ac170ae8129779b5c56d
Steven Hedges <steven.hedges34@gmail.com>
2020-10-08 10:50:23 -0400

    add game loop. Square falls off screen.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ let context = canvas.getContext("2d"); // rendering context
 const CANVAS_WIDTH = 400;
 const CANVAS_HEIGHT = 400;
 
-context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-
 // let scale = 10;
 
 let snake = new Snake(CANVAS_WIDTH, CANVAS_HEIGHT);
-snake.draw(context);
+let lastTime = 0;
 
+function gameLoop(timeStamp) {
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
 
+    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    snake.update(deltaTime);
+    snake.draw(context);
 
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
--- src/snake.js
+++ src/snake.js
@@ -6,11 +6,17 @@ export class Snake {
 
         this.snakePosition = { 
             x: canvasWidth / 2 - this.snakeWidth / 2, 
-            y: canvasWidth / 2 - this.snakeWidth / 2
+            y: canvasHeight / 2 - this.snakeHeight / 2
         }
     }
-    draw (context) {
+
+    draw(context) {
         context.fillStyle = "#000000";
         context.fillRect(this.snakePosition.x, this.snakePosition.y, this.snakeWidth, this.snakeHeight)        
+    }
+
+    update(deltaTime) {
+        if(!deltaTime) return;
+        this.snakePosition.y += 10 / deltaTime;
     }
 }
